7be624de7a60361c6720d5a0471612dbacb01b5b
leonxlin <leonxlin@gmail.com>
2020-06-15 00:34:29 -0500

    Switch from svg to canvas. Make circles move to random targets.

--- bundle.js
+++ bundle.js
@@ -3514,35 +3514,58 @@
 
   var category10 = colors("1f77b4ff7f0e2ca02cd627289467bd8c564be377c27f7f7fbcbd2217becf");
 
-  function sayHi() { 
-    select('body').html('Hey you guuuys!');
-  }
-
   window.onload = function() {
-      sayHi();
 
-      var width = window.innerWidth;
-      var height = window.innerHeight;
+      // Copied in part
+      // from https://stackoverflow.com/questions/44055869/converting-collision-detection-example-to-from-v3-to-v4-d3
 
-      var nodes = sequence(200).map(function() {
+      var canvas = document.querySelector("canvas"),
+          context = canvas.getContext("2d"),
+          width = canvas.width,
+          height = canvas.height;
+
+      var color = ordinal().range(category10);
+      var nodes = sequence(200).map(function(i) {
               return {
-                  r: Math.random() * 12 + 4
+                  r: Math.random() * 12 + 4,
+                  x: 0,
+                  y: 0,
+                  fillColor: color(i % 10)
               };
           }),
           root = nodes[0];
-      var color = ordinal().range(category10);
 
       root.radius = 0;
-      root.fixed = true;
 
       const forceX = x$1(width / 2).strength(0.015);
       const forceY = y$1(height / 2).strength(0.015);
 
+      function squaredDistance(p1, p2) {
+          var dx = p1.x - p2.x,
+              dy = p1.y - p2.y;
+          return dx * dx + dy * dy;
+      }
+
+      function agentForce(alpha) {
+          nodes.forEach(function(n) {
+              if (!("goal" in n) || squaredDistance(n, n.goal) < 10) {
+                  n.goal = {
+                      x: Math.random() * width,
+                      y: Math.random() * height
+                  };
+              }
+              var len = Math.sqrt(squaredDistance(n, n.goal));
+              n.vx += alpha * (n.goal.x - n.x) / len;
+              n.vy += alpha * (n.goal.y - n.y) / len;
+          });
+      }
+
 
       var force = simulation()
           .velocityDecay(0.2)
-          .force("x", forceX)
-          .force("y", forceY)
+          // .force("x", forceX)
+          // .force("y", forceY)
+          .force("agent", agentForce)
           .force("collide", collide().radius(function(d) {
               if (d === root) {
                   return Math.random() * 50 + 100;
@@ -3551,30 +3574,27 @@
           }).iterations(5))
           .nodes(nodes).on("tick", ticked);
 
-      var svg = select("body").append("svg")
-          .attr("width", width)
-          .attr("height", height);
-
-      svg.selectAll("circle")
-          .data(nodes.slice(1))
-          .enter().append("circle")
-          .attr("r", function(d) {
-              return d.r;
-          })
-          .style("fill", function(d, i) {
-              return color(i % 10);
-          });
 
       function ticked(e) {
-          svg.selectAll("circle")
-              .attr("cx", function(d) {
-                  return d.x;
-              })
-              .attr("cy", function(d) {
-                  return d.y;
-              });
+
+          context.clearRect(0, 0, width, height);
+          context.save();
+
+          nodes.forEach(function(d) {
+              if (d === root) return;
+
+              context.beginPath();
+              context.moveTo(d.x + d.r, d.y);
+              context.arc(d.x, d.y, d.r, 0, 2 * Math.PI);
+              context.fillStyle = d.fillColor;
+              context.fill();
+              context.strokeStyle = "#333";
+              context.stroke();
+          });
+
+          context.restore();
       }
-      svg.on("mousemove", function() {
+      select("canvas").on("mousemove", function() {
           var p1 = mouse(this);
           root.fx = p1[0];
           root.fy = p1[1];
--- js/index.js
+++ js/index.js
@@ -4,30 +4,56 @@ import * as d3 from 'd3';
 window.onload = function() {
     sayHi();
 
-    // Copied from https://stackoverflow.com/questions/44055869/converting-collision-detection-example-to-from-v3-to-v4-d3
+    // Copied in part
+    // from https://stackoverflow.com/questions/44055869/converting-collision-detection-example-to-from-v3-to-v4-d3
 
-    var width = window.innerWidth;
-    var height = window.innerHeight;
+    var canvas = document.querySelector("canvas"),
+        context = canvas.getContext("2d"),
+        width = canvas.width,
+        height = canvas.height;
 
-    var nodes = d3.range(200).map(function() {
+    var color = d3.scaleOrdinal().range(d3.schemeCategory10);
+    var nodes = d3.range(200).map(function(i) {
             return {
-                r: Math.random() * 12 + 4
+                r: Math.random() * 12 + 4,
+                x: 0,
+                y: 0,
+                fillColor: color(i % 10)
             };
         }),
         root = nodes[0];
-    var color = d3.scaleOrdinal().range(d3.schemeCategory10);
 
     root.radius = 0;
-    root.fixed = true;
 
     const forceX = d3.forceX(width / 2).strength(0.015);
     const forceY = d3.forceY(height / 2).strength(0.015);
 
+    function squaredDistance(p1, p2) {
+        var dx = p1.x - p2.x,
+            dy = p1.y - p2.y;
+        return dx * dx + dy * dy;
+    }
+
+    function agentForce(alpha) {
+        nodes.forEach(function(n) {
+            if (!("goal" in n) || squaredDistance(n, n.goal) < 10) {
+                n.goal = {
+                    x: Math.random() * width,
+                    y: Math.random() * height
+                };
+            }
+            var len = Math.sqrt(squaredDistance(n, n.goal));
+            n.vx += alpha * (n.goal.x - n.x) / len;
+            n.vy += alpha * (n.goal.y - n.y) / len;
+        })
+    }
+
 
     var force = d3.forceSimulation()
         .velocityDecay(0.2)
-        .force("x", forceX)
-        .force("y", forceY)
+        // .force("x", forceX)
+        // .force("y", forceY)
+        .force("agent", agentForce)
         .force("collide", d3.forceCollide().radius(function(d) {
             if (d === root) {
                 return Math.random() * 50 + 100;
@@ -36,31 +62,28 @@ window.onload = function() {
         }).iterations(5))
         .nodes(nodes).on("tick", ticked);
 
-    var svg = d3.select("body").append("svg")
-        .attr("width", width)
-        .attr("height", height);
-
-    svg.selectAll("circle")
-        .data(nodes.slice(1))
-        .enter().append("circle")
-        .attr("r", function(d) {
-            return d.r;
-        })
-        .style("fill", function(d, i) {
-            return color(i % 10);
-        });
 
     function ticked(e) {
-        svg.selectAll("circle")
-            .attr("cx", function(d) {
-                return d.x;
-            })
-            .attr("cy", function(d) {
-                return d.y;
-            });
+
+        context.clearRect(0, 0, width, height);
+        context.save();
+
+        nodes.forEach(function(d) {
+            if (d === root) return;
+
+            context.beginPath();
+            context.moveTo(d.x + d.r, d.y);
+            context.arc(d.x, d.y, d.r, 0, 2 * Math.PI);
+            context.fillStyle = d.fillColor;
+            context.fill();
+            context.strokeStyle = "#333";
+            context.stroke();
+        });
+
+        context.restore();
     };
 
-    svg.on("mousemove", function() {
+    d3.select("canvas").on("mousemove", function() {
         var p1 = d3.mouse(this);
         root.fx = p1[0];
         root.fy = p1[1];
