5bc500fddbec3f3f05d7ef95129c15859363e74f
Leon Lin <leonxlin@gmail.com>
2021-04-11 18:58:29 -0400

    Add basic automatic differentiation

--- js/ad.test.ts
+++ js/ad.test.ts
@@ -1,5 +1,65 @@
-import { sum } from "./ad";
+import {
+  Dual,
+  square,
+  add,
+  subtract,
+  neg,
+  mult,
+  div,
+  inv,
+  sqrt,
+  pow,
+  val,
+  ddx,
+  ddy,
+} from "./ad";
+import * as ad from "./ad";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+test("differentiates (4x + y + 23)^2", () => {
+  function f(x: number, y: number): Dual {
+    return square(add(add(mult(ad.x(x), 4), ad.y(y)), 23));
+  }
+  function dfdx(x: number, y: number): number {
+    return 2 * (4 * x + y + 23) * 4;
+  }
+  function dfdy(x: number, y: number): number {
+    return 2 * (4 * x + y + 23);
+  }
+
+  expect(f(3, 8)).toEqual([1849, dfdx(3, 8), dfdy(3, 8)]);
+});
+
+test("differentiates 72 / sqrt(xy)", () => {
+  function f(x: number, y: number): Dual {
+    return div(72, sqrt(mult(ad.x(x), ad.y(y))));
+  }
+  function dfdx(x: number, y: number): number {
+    return (-72 / 2) * Math.pow(x * y, -3 / 2) * y;
+  }
+  function dfdy(x: number, y: number): number {
+    return (-72 / 2) * Math.pow(x * y, -3 / 2) * x;
+  }
+
+  expect(f(4, 9)).toEqual([12, dfdx(4, 9), dfdy(4, 9)]);
+});
+
+test("differentiates - x^6 - 1/y", () => {
+  function f(x: number, y: number): Dual {
+    return subtract(neg(pow(ad.x(x), 6)), inv(ad.y(y)));
+  }
+  function dfdx(x: number, y: number): number {
+    return -6 * Math.pow(x, 5);
+  }
+  function dfdy(x: number, y: number): number {
+    return 1 / y / y;
+  }
+
+  const result = f(2, 10);
+  expect(val(result)).toBeCloseTo(-64.1);
+  expect(ddx(result)).toBeCloseTo(dfdx(2, 10));
+  expect(ddy(result)).toBeCloseTo(dfdy(2, 10));
+});
+
+test("differentiates 4+2", () => {
+  expect(add(4, 2)).toEqual([6, 0, 0]);
 });
--- js/ad.ts
+++ js/ad.ts
@@ -1,3 +1,86 @@
-export function sum(a: number, b: number): number {
-  return a + b;
+// A simple utility for automatic differentiation by two variables x and y.
+
+// This type contains the value of a function, the derivative of the function with respect to x, and the derivative with respect to y.
+export type Dual = [number, number, number];
+
+// Access components of Dual.
+
+export function val(a: Dual): number {
+  return a[0];
+}
+
+export function ddx(a: Dual): number {
+  return a[1];
+}
+
+export function ddy(a: Dual): number {
+  return a[2];
+}
+
+// Create basic dual numbers.
+
+// c for constant.
+export function c(c: number): Dual {
+  return [c, 0, 0];
+}
+export function x(c: number): Dual {
+  return [c, 1, 0];
+}
+export function y(c: number): Dual {
+  return [c, 0, 1];
+}
+
+// Operations.
+
+export function add(a: Dual | number, b: Dual | number): Dual {
+  if (typeof a === "number") a = c(a);
+  if (typeof b === "number") b = c(b);
+  return [a[0] + b[0], a[1] + b[1], a[2] + b[2]];
+}
+
+export function subtract(a: Dual | number, b: Dual | number): Dual {
+  if (typeof a === "number") a = c(a);
+  if (typeof b === "number") b = c(b);
+  return [a[0] - b[0], a[1] - b[1], a[2] - b[2]];
+}
+
+export function neg(a: Dual): Dual {
+  return [-a[0], -a[1], -a[2]];
+}
+
+export function mult(a: Dual | number, b: Dual | number): Dual {
+  if (typeof a === "number") a = c(a);
+  if (typeof b === "number") b = c(b);
+  return [a[0] * b[0], a[1] * b[0] + a[0] * b[1], a[2] * b[0] + a[0] * b[2]];
+}
+
+export function div(a: Dual | number, b: Dual | number): Dual {
+  if (typeof a === "number") a = c(a);
+  if (typeof b === "number") b = c(b);
+  const denom = b[0] * b[0];
+  return [
+    a[0] / b[0],
+    (a[1] * b[0] - a[0] * b[1]) / denom,
+    (a[2] * b[0] - a[0] * b[2]) / denom,
+  ];
+}
+
+export function inv(a: Dual): Dual {
+  const invsq = -1 / (a[0] * a[0]);
+  return [1 / a[0], invsq * a[1], invsq * a[2]];
+}
+
+export function pow(a: Dual, n: number): Dual {
+  const p = n * Math.pow(a[0], n - 1);
+  return [Math.pow(a[0], n), p * a[1], p * a[2]];
+}
+
+export function square(a: Dual): Dual {
+  return mult(a, a);
+}
+
+export function sqrt(a: Dual): Dual {
+  const s = Math.sqrt(a[0]);
+  const r = 0.5 / s;
+  return [s, a[1] * r, a[2] * r];
 }
