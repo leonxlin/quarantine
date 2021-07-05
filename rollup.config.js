3eae4435f5aa67d00bac5b0a9f2d45862340a72a
Leon Lin <leonxlin@gmail.com>
2021-07-04 21:18:27 -0400

    Add minified version of js.

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,27 @@
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: "js/index.ts",
-  output: {
-    file: "bundle.js",
-    format: "iife",
-    name: "quarantine",
-  },
+  output: [
+    {
+      file: "bundle.js",
+      format: "iife",
+      name: "quarantine",
+    },
+    {
+      file: "bundle.min.js",
+      format: "iife",
+      name: "quarantine_min",
+      plugins: [
+        terser({
+          mangle: {
+            properties: true,
+          },
+        }),
+      ],
+    },
+  ],
   plugins: [resolve(), typescript()],
 };
